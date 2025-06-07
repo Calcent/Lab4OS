4ec8fe3101af3b39f83ddd6b76edae532cd5d707
Calcent <134114315+Calcent@users.noreply.github.com>
2025-06-06 20:20:28 -0700

    added open and read file functions

--- Lab4OS/libFS.c
+++ Lab4OS/libFS.c
@@ -12,3 +12,22 @@ int fileCreate(const char* filename) {
 	}
 	return handle;
 }
+
+//opens file
+int fileOpen(const char* filename) {
+	int handle = TFS_open(filename);
+	if (handle < 0) {
+		fprint(stderr, "fileOpen(%s) failed: %s\n", filename, TFS_strerror(errno));
+	}
+	return handle;
+}
+
+//reads file
+ssize_t fileRead(int fd, void* buf, size_t count) {
+	ssize_t n = TFS_read(fd, buf, count);
+	if (n < 0) {
+		fprintf(stderr, "fileRead(fd=%d) failed: %s\n", fd, TFS_strerror(errno));
+	}
+	return n;
+}
+
